9496d0b67f431813341034db35bb216bb56e3c0c
ekalyenci <116082363+ekalyenci@users.noreply.github.com>
2022-11-02 22:35:16 +0300

    Add files via upload

---
+++ Binary Tree.cpp
@@ -0,0 +1,29 @@
+[7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aþamalarýný yazýnýz.
+Ýkili arama aðacý, her düðümün solundaki koldan ulaþýlabilecek bütün verilerin düðümün deðerinden küçük, sað kolundan ulaþýlabilecek verilerin deðerinin o düðümün deðerinden büyük olmasýný þart koþar.
+Açýklama			
+root=7	
+5 sayýsý 7'den küçük olduðunda 7'nin soluna ekledik
+	
+1 sayýsý 5'ten ve 7'den küçük olduðunda 7 ve 5'in soluna ekledik
+
+				
+8 sayýsý 7'den büyük olduðunda 7'nin saðýna ekledik
+
+		
+3 sayýsý 7'den ve 5'ten küçük olduðunda 5'in soluna, 1'den büyük olduðunda 1'in saðýna ekledik
+							
+			
+6 sayýsý 7'den küçük olduðunda 7'nin soluna, 5'ten büyük olduðunda 5'in saðýna ekledik
+
+
+0 sayýsý 7'den, 5'ten ve 1'den küçük olduðunda 1'in soluna ekledik
+
+			
+9 sayýsý 7'den ve 8'den büyük olduðunda 8'in saðýna ekledik
+
+	
+4 sayýsý 7'den ve 5'ten küçük olduðunda 5'in soluna, 1'den ve 3'ten büyük olduðunda 3'ün saðýna ekledik
+
+			
+2 sayýsý 7'den ve 5'ten küçük olduðunda 5'in soluna, 1'den büyük olduðunda 1'in saðýna ve 3'ten küçük olduðunda 3'ün soluna ekledik
+	
