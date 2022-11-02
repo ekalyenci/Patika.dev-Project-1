bea854c847e89609c8fb835b688883bb497b07eb
ekalyenci <116082363+ekalyenci@users.noreply.github.com>
2022-11-02 22:21:57 +0300

    Add files via upload

---
+++ Merge sort Project.cpp
@@ -0,0 +1,17 @@
+Project-2
+
+[16,21,11,8,12,22]
+
+1. Merge Sort Process
+
+First Step: [16,21,11] [8,12,22]
+Second Step: [16,21] [11] [8,12] [22]
+Third Step: [16] [21] [11] [8] [12] [22]
+Fourth Step: [16] [11,21] [8] [12,22]
+Fifth Step: [11,16,21] [8,12,22]
+Sixth Step: [8,11,12,16,21,22]
+
+2. Big O Notation
+
+For Merge Sort: 2^x=n --> logn
+=> Big O: O(logn)
